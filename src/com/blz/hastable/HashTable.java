f1eac6e32e7f1ded0f9de0bebb901b6d930b06a5
Him5696 <himanshi1252@gmail.com>
2022-07-12 22:36:01 +0530

    Frequency of Word

--- src/com/blz/hastable/HashTable.java
+++ src/com/blz/hastable/HashTable.java
@@ -1,7 +1,25 @@
 package com.blz.hastable;
 
+import java.util.Hashtable;
+
 public class HashTable {
     public static void main(String[] args) {
         System.out.println("Welcome to Hash Tables Program");
+        Hashtable<String, Integer> hashtable = new Hashtable<>();
+        String message = "To be or not to be";
+
+        String[] messageArray = message.toLowerCase().split(" ");
+
+        for (String word : messageArray){
+            Integer value = hashtable.get(word);
+
+            if (value == null)
+                value = 1;
+            else
+                value = value + 1;
+            hashtable.put(word, value);
+        }
+        System.out.println(hashtable);
+
     }
 }
